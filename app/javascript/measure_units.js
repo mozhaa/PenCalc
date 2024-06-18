89b5e82a02c278454c705a72db5ddd3e83b6e532
mozhaa <mozhay2005@gmail.com>
2024-06-18 22:50:02 +0300

    fix bug with whitespaces

--- app/javascript/measure_units.js
+++ app/javascript/measure_units.js
@@ -1,32 +1,24 @@
-jQuery.fn.visible = function() {
-    return this.css('visibility', 'visible');
-};
-
-jQuery.fn.invisible = function() {
-    return this.css('visibility', 'hidden');
-};
-
 $(function() {
     let index = 0
     $(".show-measure-units").replaceWith(function() {
         index++
-        var new_input = $(this).clone().attr("data-span-id", index).wrap("<div></div>").parent().html()
+        var new_input = $(this).clone().attr("data-pre-id", index).wrap("<div></div>").parent().html()
         console.log(new_input)
         return `
 <div style="position: relative">
     ${new_input}
     <div style="position: absolute; left: 0; top: 0; pointer-events: none">
-        <span id="mu-hidden-${index}" style="visibility: hidden"></span><span id="mu-units-${index}" style="visibility: hidden; overflow: hidden"> ${$(this).attr("data-units")}</span>
+        <pre id="mu-hidden-${index}" style="visibility: hidden; display: inline"></pre><pre id="mu-units-${index}" style="display: none; overflow: hidden"> ${$(this).attr("data-units")}</pre>
     </div>
 </div>`
     })
     $(".show-measure-units").on("input", function() {
-        var index = $(this).attr("data-span-id")
+        var index = $(this).attr("data-pre-id")
         var text = $(this).val()
         $(`#mu-hidden-${index}`).text(text)
         if (text.length)
-            $(`#mu-units-${index}`).visible()
+            $(`#mu-units-${index}`).css('display', 'inline')
         else
-            $(`#mu-units-${index}`).invisible()
+            $(`#mu-units-${index}`).css('display', 'none')
     })
 })
