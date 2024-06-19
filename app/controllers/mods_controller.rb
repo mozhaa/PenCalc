fb481fdd8cbb66759c3aba791753f355e77ab7d4
mozhaa <mozhay2005@gmail.com>
2024-06-19 16:30:42 +0300

    create action for mods

--- app/controllers/mods_controller.rb
+++ app/controllers/mods_controller.rb
@@ -2,10 +2,15 @@ class ModsController < ApplicationController
   layout "with_navbar"
 
   def new
-    @structure = if params[:structure].present? then params[:structure] else "[]" end
+    @structure = if params[:mod].present? then Mod.find_by(name: params[:mod]).structure else "[]" end
   end
 
-  def index
+  def create
+    @mod = Mod.new(name: params[:name], structure: params[:structure], author_user: @current.user.id)
+    @mod.save
+  end
+
+  def show
     @mods = Mod.all
   end
 end
