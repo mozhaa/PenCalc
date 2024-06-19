0dcbc5c3cec1a48854bd49ce182fbcefeca64c2a
mozhaa <mozhay2005@gmail.com>
2024-06-19 16:30:23 +0300

    change route mods#index to show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root :to => redirect("mods")
-  get :mods, to: "mods#index"
-  resource :mods, only: :new
+  resource :mods, only: [:new, :create, :show]
   resource :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   get "/users/:username", to: "users#show", as: :user
